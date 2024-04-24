ec508e23e7ba67df5fb47c27fce7b64e2efd27ed
user-oleksandr <name.oleksandr@gmail.com>
2024-04-24 15:15:34 +0300

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,46 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './Header.css';
 
 const Header = () => {
+    const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
+
+    const toggleSidePanel = () => {
+        setIsSidePanelOpen(!isSidePanelOpen);
+    };
+
+    const closeSidePanel = () => {
+        setIsSidePanelOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isSidePanelOpen && !event.target.closest('.side-panel') && !event.target.closest('.toggle-button')) {
+                setIsSidePanelOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isSidePanelOpen]);
+
     return (
-        <nav className="navbar navbar-dark fixed-top" style={{ height: '5.5em', backgroundColor: '#21252B' }}>
-            <div className="container-fluid test d-flex">
-                <a className="navbar-brand" style={{ color: 'orangered' }}>Soft-Age-Service</a>
-                <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="offcanvas offcanvas-end rounded shadow" tabIndex="-1"
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel" style={{ width: '275px', backgroundColor: '#21252B' }}>
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title text-light" id="offcanvasNavbarLabel" style={{ color: 'orangered' }}>Soft-Age-Service</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas"
-                            aria-label="Close"></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/" activeclassname="active">
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/about" activeclassname="active">
-                                    About
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/contacts" activeclassname="active">
-                                    Contacts
-                                </NavLink>
-                            </li>
+        <nav className="navbar">
+            <div className="nav-container">
+                <div className="title">Soft-Age-Service</div>
+                <button className="toggle-button" onClick={toggleSidePanel}>☰</button>
+                {isSidePanelOpen && (
+                    <div className="side-panel">
+                        <button className="close-button" onClick={closeSidePanel}>Х</button>
+                        <ul>
+                            <li><NavLink to="/" onClick={(e) => { e.stopPropagation(); }}>Home</NavLink></li>
+                            <li><NavLink to="/about" onClick={(e) => { e.stopPropagation(); }}>About</NavLink></li>
+                            <li><NavLink to="/contacts" onClick={(e) => { e.stopPropagation(); }}>Contacts</NavLink></li>
+
                         </ul>
                     </div>
-                </div>
+                )}
             </div>
         </nav>
     );
